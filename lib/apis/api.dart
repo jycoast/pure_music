303f22f7c1451c134481531fd28af447c56f6ad4
jiyongchao <jiyongchaowy@163.com>
2022-08-30 23:50:18 +0800

    增加搜索

--- lib/apis/api.dart
+++ lib/apis/api.dart
@@ -3,12 +3,9 @@ import 'dart:io';
 import 'package:http/http.dart';
 import 'package:http/io_client.dart';
 import 'dart:convert';
-
-import 'package:pure_music/widgets/music_item.dart';
-
 import '../model/music_model.dart';
 
-class MusicAPI {
+class KwMusicAPI {
   Map<String, String> headers = {};
   String baseUrl = 'http://www.kuwo.cn/api';
   String apiStr = '/api.php?_format=json&_marker=0&api_version=4&ctx=web6dot0';
